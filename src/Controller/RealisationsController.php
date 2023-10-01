3673326de7640c90e12e4194a0c096be18737589
unknown <helen.allouch@gmail.com>
2023-10-01 22:41:20 +0200

    style page home et realisation et services

--- src/Controller/RealisationsController.php
+++ src/Controller/RealisationsController.php
@@ -26,4 +26,17 @@ class RealisationsController extends AbstractController
             'realisation' => $realisation,
         ]);
     }
+
+    // example de route pour faire variabiliser les img in Realisations
+    // pour ça il faut mettre les images dans la bdd pour chaque real et apres 
+    //le site pourra afficher chaque image 
+    // 
+
+    // #[Route('/realisations', name: 'app_realisations')]
+    // public function index(): Response
+    // {
+    //     return $this->render('realisations/index.html.twig', [
+    //         'controller_name' => 'RealisationsController',
+    //     ]);
+    // }
 }
